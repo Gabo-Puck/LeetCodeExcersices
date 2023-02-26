bac51cc900a85f06fe1b56c79ca62b27b939c2f3
Gabo-Puck <melkorgodo@gmail.comk>
2023-02-26 01:15:23 -0600

    Added new excersices

---
+++ steps_reduce_number_zero/steps_reduce_number_zero.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int numberOfSteps(int num)
+{
+    int steps = 0;
+    while (num > 0)
+    {
+        // 0000 0
+        // 0001 1
+        // 0010 2
+        // 0011 3
+        // 0100 4
+        // 0101 5
+        // 0110 6
+
+        if ((num & 1) == 0)
+            num = num >> 1;
+        else
+            num--;
+        steps++;
+    }
+    return steps;
+}
+int main()
+{
+    std::cout << numberOfSteps(8);
+}
